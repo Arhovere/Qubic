ce21ab46599c5b007d2634ec17d564f30b4c03dc
Arhovere <abrflake@gmail.com>
2017-02-01 17:17:13 +0100

    Implementation winner-methods, except for hasDiagonal()

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -2,6 +2,8 @@ package Board;
 
 import java.util.Arrays;
 
+import Exceptions.CoordinatesNotFoundException;
+
 /**
  * This is the game board for a Qubic game.
  * 
@@ -13,10 +15,11 @@ public class Board {
 	private Mark[][][] fields;
 
 	public static final int DIM = 4;
-	private static final String[] RASTER = { "___|___|___", "---+---+---", "   |   |   ", "---+---+---",
-			"   |   |   " };
-	private static final String LINE = RASTER[1];
-	private static final String SPACE = "     ";
+	// private static final String[] RASTER = { "___|___|___", "---+---+---", "
+	// | | ", "---+---+---",
+	// " | | " };
+	// private static final String LINE = RASTER[1];
+	// private static final String SPACE = " ";
 
 	// -----Constructor----------------------------------------------
 
@@ -61,18 +64,160 @@ public class Board {
 	 * @param lay
 	 *            number of the layer
 	 * @return Mark on the field
+	 * @throws CoordinatesNotFoundException
+	 *             if field out of range
 	 */
-	public Mark getField(int row, int col, int lay) {
+	// @ requires this.isField(row,col,lay);
+	// @ ensures \result == Mark.EMPTY || \result == Mark.XX || \result ==
+	// Mark.OO;
+	/* @pure */
+	public Mark getField(int row, int col, int lay) throws CoordinatesNotFoundException {
 		if (isField(row, col, lay)) {
 			Mark res = fields[row][col][lay];
 			return res;
 		} else {
-			return null;
+			throw new CoordinatesNotFoundException("Coordinate(s) of the board are out of range.");
 		}
 	}
 
-	public void setField(Mark m) {
-		// to do
+	/**
+	 * Checks if the content of the field referred to is empty.
+	 * 
+	 * @param row
+	 *            no of the row
+	 * @param col
+	 *            no of the column
+	 * @param lay
+	 *            no of the layer
+	 * @return true if getField(row, col, lay) == Mark.EMPTY
+	 * @throws CoordinatesNotFoundException
+	 *             if field out of range
+	 */
+	// @ requires this.isField(row,col,lay);
+	// @ ensures \result == (this.getField(row,col,lay) == Mark.EMPTY);
+	/* @pure */
+	public boolean isEmptyField(int row, int col, int lay) throws CoordinatesNotFoundException {
+		return (getField(row, col, lay) == Mark.EMPTY);
+	}
+
+	/**
+	 * Checks if the same spot on the layer below is empty, if there is one.
+	 * 
+	 * @param row
+	 *            no of the row
+	 * @param col
+	 *            no of the column
+	 * @param lay
+	 *            no of the layer
+	 * @return true if lay == 0 || !(isEmptyField(row, col, (lay - 1)))
+	 * @throws CoordinatesNotFoundException
+	 *             if field is out of range
+	 */
+	// @ requires this.isField(row,col,lay);
+	// @ ensures \result (this.getField(row,col,(lay-1)) != Mark.EMPTY);
+	/* @pure */
+	public boolean belowIsNotEmpty(int row, int col, int lay) throws CoordinatesNotFoundException {
+		if (lay == 0) {
+			return true;
+		} else if (!(isEmptyField(row, col, (lay - 1)))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Sets the mark on the field referred to by the parameters to the given
+	 * Mark.
+	 * 
+	 * @param row
+	 *            no of the row
+	 * @param col
+	 *            no of the column
+	 * @param lay
+	 *            no of the layer
+	 * @param m
+	 *            the Mark to conquer this field with
+	 * @throws CoordinatesNotFoundException
+	 *             if field is out of range
+	 */
+	// @ requires this.isField(row,col,lay);
+	// @ ensures this.getField == m;
+	public void setField(int row, int col, int lay, Mark m) throws CoordinatesNotFoundException {
+		if (isEmptyField(row, col, lay) && belowIsNotEmpty(row, col, lay)) {
+			fields[row][col][lay] = m;
+		}
+	}
+
+	public boolean hasRow(Mark m) throws CoordinatesNotFoundException {
+		for (int l = 0; l < DIM; l++) {
+			for (int r = 0; r < DIM; r++) {
+				boolean res = true;
+				for (int c = 0; c < DIM; c++) {
+					if (!(getField(r, c, l) == m)) {
+						res = false;
+						break;
+					}
+				}
+				if (res) {
+					return res;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean hasColumn(Mark m) throws CoordinatesNotFoundException {
+		for (int l = 0; l < DIM; l++) {
+			for (int c = 0; c < DIM; c++) {
+				boolean res = true;
+				for (int r = 0; r < DIM; r++) {
+					if (!(getField(r, c, l) == m)) {
+						res = false;
+						break;
+					}
+				}
+				if (res) {
+					return res;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean hasTower(Mark m) throws CoordinatesNotFoundException {
+		for (int r = 0; r < DIM; r++) {
+			for (int c = 0; c < DIM; c++) {
+				boolean res = true;
+				for (int l = 0; l < DIM; l++) {
+					if (!(getField(r, c, l) == m)) {
+						res = false;
+						break;
+					}
+				}
+				if (res) {
+					return res;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean hasDiagonal(Mark m) {
+		return false;
+	}
+
+	public boolean isWinner(Mark m) {
+		try {
+			return (hasRow(m) || hasColumn(m) || hasDiagonal(m) || hasTower(m));
+		} catch (CoordinatesNotFoundException e) {
+			System.out.println("Coordinates not found or out of range.");
+		}
+		return false;
+	}
+
+	public boolean hasWinner() {
+		return (isWinner(Mark.OO) || isWinner(Mark.XX));
 	}
 
 	/**
---
+++ src/Exceptions/CoordinatesNotFoundException.java
@@ -0,0 +1,18 @@
+package Exceptions;
+
+public class CoordinatesNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CoordinatesNotFoundException(String string) {
+		super(string);
+	}
+
+	public CoordinatesNotFoundException() {
+
+	}
+
+}
