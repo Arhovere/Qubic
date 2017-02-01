7e71325b432eb98830ec45514fbceb82be5cae6b
Arhovere <abrflake@gmail.com>
2017-02-01 14:09:38 +0100

    Implemented Mark, Board stil empty

---
+++ src/Board/Mark.java
@@ -0,0 +1,35 @@
+package Board;
+
+/**
+ * Represents a mark in the Qubic game. There are three possible values:
+ * Mark.EMPTY, Mark.XX and Mark.OO.
+ * 
+ * @author beitske
+ *
+ */
+public enum Mark {
+
+	EMPTY, XX, OO;
+
+	
+    /*@
+    ensures this == Mark.XX ==> \result == Mark.OO;
+    ensures this == Mark.OO ==> \result == Mark.XX;
+    ensures this == Mark.EMPTY ==> \result == Mark.EMPTY;
+  */
+	/**
+	 * Returns the opposite Mark, or empty.
+	 * 
+	 * @return The other Mark, that is not EMPTY.
+	 */
+	public Mark reverse() {
+		if (this == XX) {
+			return OO;
+		} else if (this == OO) {
+			return XX;
+		} else {
+			return EMPTY;
+		}
+	}
+
+}
